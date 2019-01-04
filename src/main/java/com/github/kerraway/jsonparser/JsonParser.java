771d3e1ec48d53e46f29f43cb4f280e8b25b8647
kerraway <geekxiaoke@gmail.com>
2019-01-04 11:49:42 +0800

    init

---
+++ src/main/java/com/github/kerraway/jsonparser/JsonParser.java
@@ -0,0 +1,8 @@
+package com.github.kerraway.jsonparser;
+
+/**
+ * @author kerraway
+ * @date 2019/1/4
+ */
+public class JsonParser {
+}
---
+++ src/test/java/com/github/kerraway/jsonparser/JsonParserTests.java
@@ -0,0 +1,14 @@
+package com.github.kerraway.jsonparser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author kerraway
+ * @date 2019/1/4
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+})
+public class JsonParserTests {
+}
